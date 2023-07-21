4f3f2c4e0eb8fc24c5d26a92d8b4729e8266e06c
akramjonnuriddinov <akramjonmohirdev@gmail.com>
2023-07-21 21:17:27 +0500

    Add loadLocale

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,38 @@
-<template>
-  <router-view />
-</template>
+<script setup>
+import '@formatjs/intl-relativetimeformat/polyfill'
+import '@formatjs/intl-relativetimeformat/locale-data/en'
+import '@formatjs/intl-relativetimeformat/locale-data/de'
 
-<script setup lang="ts"></script>
+const unsupportedLocales = ['uz']
+
+async function loadLocale(locale) {
+  // This locale is supported
+  if (!unsupportedLocales.includes(locale)) {
+    return
+  }
+  // Load the polyfill 1st BEFORE loading data
+  await import('@formatjs/intl-datetimeformat/polyfill-force')
+  // Parallelize CLDR data loading
+  const dataPolyfills = [
+    import('@formatjs/intl-datetimeformat/add-all-tz'),
+    import(`@formatjs/intl-datetimeformat/locale-data/${locale}.js`),
+  ]
+  await Promise.all(dataPolyfills)
+}
+
+loadLocale('uz').then(() => {
+  const formatter = new Intl.DateTimeFormat('uz', { month: 'long' })
+  const month1 = formatter.format(new Date())
+  const month2 = formatter.format(new Date(2003, 5, 12))
+  console.log(`${month1} and ${month2}`)
+  localStorage.setItem('months', JSON.stringify([month1, month2]))
+})
+const month = JSON.parse(localStorage.getItem('months'))
+</script>
+
+<template>
+  <div>
+    {{ month }}
+    <router-view />
+  </div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,22 @@ import './input.css'
 import { createPinia } from 'pinia'
 import UUID from "vue-uuid"
 
+import { createIntl } from 'vue-intl'
+
 
 const pinia = createPinia()
 const app = createApp(App)
 app.use(pinia)
 
 app.use(UUID)
+app.use(
+  createIntl({
+    locale: 'en',
+    defaultLocale: 'en',
+    messages: {
+      foo: 'bar',
+    },
+  })
+)
+
 createApp(App).use(router).mount('#app')
